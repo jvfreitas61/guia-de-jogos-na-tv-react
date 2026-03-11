62c9d92baed80ba4568095edca37ee7125e02175
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2026-03-11 16:52:42 -0300

    Update api.js

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:3000";
+const URL = import.meta.env.VITE_API_URL;
 
 const responseCampeonatos = await axios.get(`${URL}/campeonatos`);
 const responseJogos = await axios.get(`${URL}/jogos`);
